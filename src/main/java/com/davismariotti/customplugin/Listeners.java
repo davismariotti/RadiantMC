f68a622c80007ecba5f36f33a497c081847d0b6e
Davis Mariotti <davis@hintmd.com>
2020-04-24 12:05:55 -0700

    Add more features
    # Conflicts:
    #	src/main/resources/config.yml

--- src/main/java/com/davismariotti/customplugin/Listeners.java
+++ src/main/java/com/davismariotti/customplugin/Listeners.java
@@ -2,6 +2,7 @@ package com.davismariotti.customplugin;
 
 import com.davismariotti.customplugin.util.SendGridService;
 import org.bukkit.ChatColor;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -11,7 +12,9 @@ import org.bukkit.event.player.PlayerJoinEvent;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.util.List;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.*;
 
 public class Listeners implements Listener {
 
@@ -30,25 +33,34 @@ public class Listeners implements Listener {
             e.printStackTrace();
         }
 
-        // Send the email
-        FileConfiguration configuration = CustomPlugin.instance.getConfig();
+        // Check the time
+        ZoneId id = ZoneId.of("America/Los_Angeles");
+        ZonedDateTime zdt = ZonedDateTime.now(id);
+        if (zdt.getHour() <= 23 && zdt.getHour() >= 7) {
+            // Send the email
+            FileConfiguration configuration = CustomPlugin.instance.getConfig();
 
-        // Find excluded
-        List<String> excludedPlayers = configuration.getStringList("email.excludedPlayers");
-        if (!excludedPlayers.contains(player.getName())) {
-            String apiKey = configuration.getString("email.apiKey");
-            List<String> recipients = configuration.getStringList("email.recipients");
-
-            SendGridService service = new SendGridService(apiKey);
-            for (String recipient : recipients) {
-                try {
-                    service.sendLoggedInEmail(recipient, player.getName());
-                } catch (IOException e) {
-                    CustomPlugin.instance.getLogger().warning("There was an exception sending an email.");
-                    e.printStackTrace();
+            // Find excluded
+            List<String> excludedPlayers = configuration.getStringList("email.excludedPlayers");
+            if (!excludedPlayers.contains(player.getName())) {
+                String apiKey = configuration.getString("email.apiKey");
+                ConfigurationSection configurationSection = configuration.getConfigurationSection("email.recipients");
+                if (configurationSection != null) {
+                    Map<String, Object> recipients = configurationSection.getValues(false);
+                    SendGridService service = new SendGridService(apiKey);
+                    for (Map.Entry<String, Object> recipient : recipients.entrySet()) {
+                        if (recipient.getKey().equals(player.getName())) continue;
+                        CustomPlugin.instance.getLogger().info(recipient.getValue().toString());
+                        try {
+                            service.sendLoggedInEmail((String) recipient.getValue(), player.getName());
+                        } catch (IOException e) {
+                            CustomPlugin.instance.getLogger().warning("There was an exception sending an email.");
+                            e.printStackTrace();
+                        }
+                    }
+                    CustomPlugin.instance.getLogger().info(String.format("Emails sent to %d recipient%s", recipients.size(), recipients.size() == 1 ? "" : "s"));
                 }
             }
-            CustomPlugin.instance.getLogger().info(String.format("Emails sent to %d recipient%s", recipients.size(), recipients.size() == 1 ? "" : "s"));
         }
     }
 
